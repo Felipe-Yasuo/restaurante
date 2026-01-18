222d1ae8336c3c98591a3f4a7ff1dd134c69a3d9
Felipe-Yasuo <felipe.yasuo@hotmail.com>
2026-01-17 22:18:12 -0300

    git commit -m"feat(auth): implementar login com validacao de senha e geracao de token jwt"

--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -1,3 +1,6 @@
+import { compare } from "bcryptjs";
+import prismaClient from "../../prisma";
+import { sign } from 'jsonwebtoken'
 
 interface AuthUserServiceProps {
     email: string;
@@ -6,9 +9,40 @@ interface AuthUserServiceProps {
 
 class AuthUserService {
     async execute({ email, password }: AuthUserServiceProps) {
-        console.log({ email, password })
+        const user = await prismaClient.user.findFirst({
+            where: {
+                email: email
+            }
+        })
 
-        return "LOGADOO!"
+        if (!user) {
+            throw new Error("Email/Senha é obrigatório")
+        }
+
+        // Verificar se a senha esta correta
+        const passwordMatch = await compare(password, user.password)
+
+        if (!passwordMatch) {
+            throw new Error("Email/Senha é obrigatório")
+        }
+
+        // GERAR TOKEN JWT
+        const token = sign({
+            name: user.name,
+            email: user.email,
+
+        }, process.env.JWT_SECRET as string, {
+            subject: user.id,
+            expiresIn: "30d"
+        })
+
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            token: token,
+        }
     }
 }
 
