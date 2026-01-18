26f277e66013efad1c624fdbac0e54a13f829c51
Felipe-Yasuo <felipe.yasuo@hotmail.com>
2026-01-18 18:29:54 -0300

    git commit -m "refactor(user): obter user_id exclusivamente do middleware de autenticacao"

--- backend/src/controllers/user/DetailUserController.ts
+++ backend/src/controllers/user/DetailUserController.ts
@@ -5,7 +5,7 @@ import { DetailUserService } from "../../services/user/DetailUserService";
 class DetailUserController {
     async handle(request: Request, response: Response) {
 
-        const { user_id } = request.body;
+        const user_id = request.user_id
 
         const detailUser = new DetailUserService();
         const user = await detailUser.execute(user_id)
