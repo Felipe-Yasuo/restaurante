347310597e16ba3f758395cbc8a167173e5f742c
Felipe-Yasuo <felipe.yasuo@hotmail.com>
2026-03-10 18:02:35 -0300

    feat: implementa estrutura inicialdo app mobile com layout, tela de login e componentes reutil

--- mobile/app/login.tsx
+++ mobile/app/login.tsx
@@ -1,9 +1,81 @@
-import { Text, View } from "react-native";
+import { colors, fontSize, spacing } from "@/constants/theme";
+import {
+    KeyboardAvoidingView,
+    Platform,
+    ScrollView,
+    StyleSheet,
+    Text,
+    View,
+} from "react-native";
+
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
 
 export default function Login() {
     return (
-        <View>
-            <Text>Pagina LOGIN</Text>
-        </View>
+        <KeyboardAvoidingView
+            style={styles.container}
+            behavior={Platform.OS === "ios" ? "padding" : undefined}
+        >
+            <ScrollView
+                contentContainerStyle={styles.scrollContent}
+                keyboardShouldPersistTaps="handled"
+            >
+                <View style={styles.logoContainer}>
+                    <Text style={styles.logoText}>
+                        Sujeito<Text style={styles.logoBrand}>Pizzaria</Text>
+                    </Text>
+                    <Text style={styles.logoSubtitle}>Garçom app</Text>
+                </View>
+
+                <View style={styles.formContainer}>
+                    <Input
+                        label="Email"
+                        placeholder="Digite seu email..."
+                        placeholderTextColor={colors.gray}
+                    />
+
+                    <Input
+                        label="Senha"
+                        placeholder="Digite sua senha..."
+                        placeholderTextColor={colors.gray}
+                        secureTextEntry={true}
+                    />
+
+                    <Button title="Acessar" />
+                </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: colors.background,
+        flex: 1,
+    },
+    scrollContent: {
+        justifyContent: "center",
+        flexGrow: 1,
+        paddingHorizontal: spacing.xl,
+    },
+    logoContainer: {
+        alignItems: "center",
+        marginBottom: spacing.xl,
+    },
+    logoText: {
+        fontSize: 34,
+        fontWeight: "bold",
+        color: colors.primary,
+    },
+    logoBrand: {
+        color: colors.brand,
+    },
+    logoSubtitle: {
+        color: colors.primary,
+        fontSize: fontSize.lg,
+    },
+    formContainer: {
+        gap: spacing.md,
+    },
+});
